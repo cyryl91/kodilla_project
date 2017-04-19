96d0060dd05e85a64da3fd3be725a482bace69c9
cyryl91 <91.cyryl@gmail.com>
2017-04-19 17:56:13 +0200

    add task watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,18 +18,27 @@ module.exports = function(grunt) {
   			files: [{
   				expand: true,
   				cwd: 'images/',
-  				src: ['**/*.{png,jpg,gif}'],
+  				src: ['**/*.{png,jpg,jpeg,gif}'],
   				dest: 'images/build/'
   			}]
   		}
-  	}
-
+  	},
+    watch: {
+    scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+    } 
+}
   });
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   
-  grunt.registerTask('default', ['sass', 'imagemin']);
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
 };
